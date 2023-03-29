5df9945a4eaae49c8c995255b0491de2d976a024
Nina Letenayova <ninalet@mail.muni.cz>
2023-03-29 21:29:15 +0200

    IT WORKS!!!

--- Assets/Scripts/CombinedSteeringAgent.cs
+++ Assets/Scripts/CombinedSteeringAgent.cs
@@ -25,16 +25,14 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
     private CombinedSteeringAgent[] otherAgents;
     private int currentPathNode = 0;
     private Vector3[] points;
-
-    private Vector3 targetPosition;
     private bool back = false;
     
     // [SerializeField] 
-    private float avoidDistance = 2f;
+    private float avoidDistance;
     // [SerializeField] 
-    private float lookahead = 1f;
+    private float lookahead;
     // [SerializeField] 
-    private float sideViewAngle = 45f;
+    private float sideViewAngle = 30f;
 
     // The AI agent can be considered a sphere
     // with center at "Position" and radius equal to this property
@@ -52,6 +50,8 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
         base.Start();
 
         InitializeObstaclesArrays();
+        lookahead = 3*Radius;
+        avoidDistance = 2.5f*Radius;
     }
 
     protected override void Update()
@@ -63,7 +63,6 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
         //      Variables "staticObstacles" and "otherAgents" contain information about obstacles to avoid, and agents to avoid.
         //      However, your solution does not necessarily have to use these arrays – but they are here to help you in case of need.
         
-        //path
         points = pathToFollow.GetPathVertices();
         if (currentPathNode == 0)
         {
@@ -73,6 +72,8 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
         {
             back = true;
         }
+        Vector3 colissionTarget = CollisionAvoidance();
+        Vector3 obstacleTarget = ObstacleAvoidance();
         
         if (Vector3.Distance(points[currentPathNode], Position) < 0.1)
         {
@@ -84,40 +85,18 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
             {
                 currentPathNode += 1;
             }
+            SetRotationImmediate(points[currentPathNode] - Position);
         }
-        targetPosition = points[currentPathNode] - Position;
-        
-        //agents
-        Vector3 colissionTarget = CollisionAvoidance();
-
-        //obstacles
-        Vector3 obstacleTarget = ObstacleAvoidance();
+        else
+            SetRotationTransition(points[currentPathNode] - Position);
 
         // final direction
-        // targetPosition = (5*targetPosition + colissionTarget + obstacleTarget)/3;
-        float obstacleDistance = Vector3.Distance(Position, ClosestObstacle().WorldCenter);
+        if (obstacleTarget != Vector3.zero)
+            SetRotationImmediate(obstacleTarget - Position);
 
-        if (obstacleDistance < ClosestObstacle().Radius + Radius+0.1f)
-        {
-            // targetPosition = obstacleTarget - Position; 
-            if (obstacleTarget != Vector3.zero)
-                targetPosition = obstacleTarget - Position;
-             // targetPosition = obstacleTarget; 
-        }
-        // else
-        // {
-            // SetRotationImmediate(targetPosition);
-        // }
-        // targetPosition = colissionTarget - Position;
-        // if (colissionTarget != Vector3.zero)
-        // {
-            // SetRotationImmediate(colissionTarget); 
-        // }
-        // else
-        // {
-            SetRotationImmediate(targetPosition); 
-        // }
-        
+        if (colissionTarget != Vector3.zero)
+            SetRotationTransition(colissionTarget);
+
         Velocity = LookDirection * maxSpeed;
     }
 
@@ -156,52 +135,67 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
         }
     }
 
-
+    private CombinedSteeringAgent GetClosestAgent()
+    {
+        CombinedSteeringAgent closest = null;
+        float closestDist = Mathf.Infinity;
+        foreach (CombinedSteeringAgent agent in otherAgents)
+        {
+            float curDist = Vector3.Distance(agent.Position, Position);
+            if (curDist < closestDist)
+            {
+                closestDist = curDist;
+                if (curDist < 3*(agent.Radius + Radius))
+                    closest = agent;
+            }
+        }
+        return closest;
+    }
     private Vector3 ObstacleAvoidance()
     {
         Vector3 target = Vector3.zero;
         Vector3[] rayVector = new Vector3[3];
-        rayVector[0] = Velocity;
-        rayVector[0].Normalize();
-        rayVector[0] *= lookahead;
-        float rayOrientation = Mathf.Atan2(  Velocity.x,  Velocity.z);
-        float rightRayOrientation = rayOrientation + (sideViewAngle *  Mathf.Deg2Rad);
-        float leftRayOrientation = rayOrientation - (sideViewAngle *  Mathf.Deg2Rad);
-        rayVector[1] = new Vector3(Mathf.Cos(rightRayOrientation), 0,  Mathf.Sin(rightRayOrientation));
-        rayVector[1].Normalize();
-        rayVector[1] *= lookahead;
-        rayVector[2] = new Vector3(Mathf.Cos(leftRayOrientation), 0,  Mathf.Sin(leftRayOrientation));
-        rayVector[2].Normalize();
-        rayVector[2] *= lookahead;
-        
+        rayVector[2] = LookDirection*lookahead;
+        rayVector[0] = Quaternion.Euler(0, -sideViewAngle, 0) * rayVector[2];
+        rayVector[1] = Quaternion.Euler(0, sideViewAngle, 0) * rayVector[2];
+
         for (int i = 0; i < rayVector.Length; i++)
         {
             RaycastHit hit;
-            if (Physics.Raycast(Position, rayVector[i], out hit, lookahead,obstacleLayer))
+            Vector3 pos = new Vector3(Position.x, 0.25f, Position.z);
+            if (Physics.Raycast(pos, rayVector[i], out hit, lookahead,obstacleLayer))
             {
                 target = hit.point + (hit.normal * avoidDistance);
-                target.Normalize();
-                break;
+                return new Vector3(target.x, 0, target.z);
             }
         }
-        return new Vector3(target.x,0,target.z);
+        return target;
     }
 
     private Vector3 CollisionAvoidance()
     {
         Vector3 target = Vector3.zero;
+
+        CombinedSteeringAgent agent = GetClosestAgent();
+        if (agent == null)
+            return target;
+        
+        
+        
+        
         float shortestTime = float.PositiveInfinity;
         CombinedSteeringAgent firstTarget = null;
         float firstMinSeparation = 0, firstDistance = 0, firstRadius = 0;
         Vector3 firstRelativePos = Vector3.zero, firstRelativeVel = Vector3.zero;
-        foreach (CombinedSteeringAgent agent in otherAgents)
-        {
+        // foreach (CombinedSteeringAgent agent in otherAgents)
+        // {
             Vector3 relativePos = Position - agent.Position;
             Vector3 relativeVel = (LookDirection - agent.LookDirection);
             float distance = relativePos.magnitude;
             float relativeSpeed = relativeVel.magnitude;
             if (relativeSpeed == 0)
-                continue;
+                // continue;
+                return target;
 
             float timeToCollision = -1 * Vector3.Dot(relativePos, relativeVel) / (relativeSpeed * relativeSpeed);
 
@@ -209,7 +203,8 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
             float minSeparation = separation.magnitude;
 
             if (minSeparation > Radius + agent.Radius)
-                continue;
+                return target;
+                // continue;
 
             if ((timeToCollision > 0) && (timeToCollision < shortestTime))
             {
@@ -221,31 +216,21 @@ public class CombinedSteeringAgent : AbstractSteeringAgent
                 firstRelativeVel = relativeVel;
                 firstRadius = agent.Radius;
             }
-        }
+        // }
 
         if (firstTarget == null)
         {
             return target;
         }
         if (firstMinSeparation <= 0 || firstDistance < Radius + firstRadius)
-            target = Position - firstTarget.Position;
-        else
-            target = firstRelativePos + firstRelativeVel * shortestTime;
-        target.Normalize();
-        return target;
-    }
-    private Sphere ClosestObstacle()
-    {
-        Sphere res = staticObstacles[0];
-        for (int i = 1; i < staticObstacles.Length; i++)
         {
-            if (Vector3.Distance(staticObstacles[i].WorldCenter, Position) <
-                Vector3.Distance(res.WorldCenter, Position))
-            {
-                res = staticObstacles[i];
-            }
+            target = Position - firstTarget.Position;
+        }
+        else
+        {
+            target = firstRelativePos + firstRelativeVel * shortestTime;
         }
 
-        return res;
+        return target;
     }
 }
